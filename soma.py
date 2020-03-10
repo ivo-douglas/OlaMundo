95197ba4b0266231005ac2eec1eee9f6b249f22e
ivo-douglas <61335796+ivo-douglas@users.noreply.github.com>
2020-03-09 20:20:55 -0400

    Progama python para converter qtd total de horas

--- soma.py
+++ soma.py
@@ -1,6 +1,11 @@
-print("Entre dois numeros inteiros para somar")
-a = int(input())
-b = int(input())
-soma = a + b
-print(soma)
+x = int(input("entre a quantidade em horas deste exato momento: "))
+                
+y = int(input("entre a quantidade em horas que você quer esperar, antes do alarme tocar: "))
+                  
+hours_sum = x + y
 
+hour_sum_to_alarm_ring_in_hours = hours_sum % 24
+
+hour_sum_to_alarm_ring_in_days= hours_sum // 24
+
+print("o alarme tocara daqui", hour_sum_to_alarm_ring_in_days, "dias e em", hour_sum_to_alarm_ring_in_hours, "horas")
